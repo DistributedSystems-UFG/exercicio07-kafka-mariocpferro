e182714c6c808c02833c22143c0c773e465ac1ac
Mário Ferro <marioferro1205@gmail.com>
2026-04-23 08:20:12 -0300

    modificação do consumer para processar cada evento e publicá-los em um novo tópico

--- consumer.py
+++ consumer.py
@@ -1,4 +1,4 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 from const import *
 import sys
 
@@ -10,10 +10,16 @@ consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 
 try:
   topic = sys.argv[1]
+  output_topic = sys.argv[2]
 except:
-  print ('Usage: python3 consumer <topic_name>')
+  print ('Usage: python3 consumer <input_topic> <output_topic>')
   exit(1)
-  
+
+producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
 consumer.subscribe([topic])
 for msg in consumer:
     print (msg.value)
+    processed = b'[PROCESSED] ' + msg.value
+    producer.send(output_topic, value=processed)
+    producer.flush()
